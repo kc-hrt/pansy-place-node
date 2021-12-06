f3a16a194171229705d0618425b11ef35ba99d4a
Kasey Hart <kasey@api.fotohut.org>
2021-12-06 12:28:20 -0600

    CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 42003;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://*.fotohut.org");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
