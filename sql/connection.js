e65bb8ffa18d23acd5f3af4c05bfdca148d85186
Kasey Hart <kasey@api.fotohut.org>
2021-12-06 02:40:12 -0600

    i dont know

--- sql/connection.js
+++ sql/connection.js
@@ -10,8 +10,8 @@ class Connection {
         connectionLimit: 100,
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_INSTANCE
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DEFAULT_SCHEMA
       }
 
       if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
