86e5e9309a8dab1ecd409b85f4ce31dd5d0e22bf
kc-hrt <kaseyh@pm.me>
2021-12-07 19:37:25 -0600

    added message for new front-end location

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 42003;
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://*.fotohut.org");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routers/crops'));
-app.use(require('./routers/locations'));
-app.use(require('./routers/plants'));
-app.use(require('./routers/pots'));
-app.use(require('./routers/users'));
-app.use(require('./routers/availability'));
-// 
-app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
-})
+app.use(require("./routers/crops"));
+app.use(require("./routers/locations"));
+app.use(require("./routers/plants"));
+app.use(require("./routers/pots"));
+app.use(require("./routers/users"));
+app.use(require("./routers/availability"));
+//
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to our server! Visit fotohut.org/pansyplace for front-end app"
+  );
+});
 
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+  console.log(`Web server is listening on port ${port}!`);
 });
