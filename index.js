801e52e69bfff00330cd16d85f00a07b2e193f67
kc-hrt <kaseyh@pm.me>
2021-12-12 19:07:37 -0600

    issue with CORS

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 42003;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+const cors = require("cors");
+app.use(cors());
+app.options("*", cors());
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
